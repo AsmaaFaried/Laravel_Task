b621af46cc445ccfb3e63a058efe0e41f3845e75
AsmaaFaried <asmaafaried54@gmail.com>
2022-07-30 12:48:00 +0200

    add Api routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Apis\AuthController;
+use App\Http\Controllers\Apis\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
+Route::post('/register',[AuthController::class,'register'])->name('register');
+
+Route::post('/login',[AuthController::class,'login'])->name('api.login');
+
+Route::get('/posts', [PostController::class, 'index'])->middleware('auth:sanctum');
